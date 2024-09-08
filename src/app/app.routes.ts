cc9ee4c627bdab5b3878d49e02f0ec4ae961fd78
Iot-Health-Br <igl_macedo@hotmail.com>
2024-09-08 11:17:05 -0300

    Código Inicial, formulário Angular.Obs:Funcionando

---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { CardComponent } from './components/card/card.component';
+
+export const routes: Routes = [
+  {
+    path:'',
+    component: CardComponent
+  }
+];
---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,37 @@
+import { Component} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule, MatCardTitle } from '@angular/material/card';
+
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+import {ChangeDetectionStrategy} from '@angular/core';
+import {MatNativeDateModule, provideNativeDateAdapter} from '@angular/material/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  providers: [provideNativeDateAdapter()],
+  imports: [
+    MatCardModule,
+    MatCardTitle,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss'
+})
+export class CardComponent {
+  value: string = '';
+  cpfValue: string = '';
+}
